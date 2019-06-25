23dfaeda70bfe083c1c56cfd1dbcc3b8fcd68fc0
Bogdan Paratskiy <bog4242@gmail.com>
2019-06-25 13:11:20 +0300

    add readme file

---
+++ lib/codebreaker_paratskiy.rb
@@ -0,0 +1,6 @@
+require "codebreaker_paratskiy/version"
+
+module CodebreakerParatskiy
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/codebreaker_paratskiy/version.rb
@@ -0,0 +1,3 @@
+module CodebreakerParatskiy
+  VERSION = "0.1.0"
+end
