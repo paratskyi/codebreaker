8bff6dc429ec664be5f9deb75e4c90865d1dc0c4
Bogdan Paratskiy <bog4242@gmail.com>
2019-09-23 19:35:35 +0300

    rewrite default init game

--- lib/codebreaker_paratskiy.rb
+++ lib/codebreaker_paratskiy.rb
@@ -1,6 +1,6 @@
 require_relative 'game'
 module CodebreakerParatskiy
-  def self.run_game(name, difficulty)
+  def self.run_game(name = DEFAULT_NAME, difficulty = DIFFICULTIES[:default])
     game = Game.new(name, difficulty)
     game.run
     game
